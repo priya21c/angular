e868c3cc34ec29a5a5ac80ce072f7c018f3d7c85
Raj Kumar Tharmaraj <rajkumart@scrunchworks.com>
2024-11-21 15:57:56 +0530

    fst

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'profile';
+  name: string = 'Mohan Doss B';
+  profession: string = 'B.Com Auditor';
+  role: string = 'Partner';
+  phone: string = '+91 8270734385';
+  email: string = 'deenadeena335@outlook.com';
+  address: string = 'NO.37/19,1st Floor, Bajanai Kovil Street, Choolaimedu, Chennai, Tamil Nadu-600 094';
+  // profileImage: string = '../assets/image/image1.jpeg';  // Assuming
 }
